f51d4765b8dd661a450725ebe00aeb2e9f6e16cb
Abdullah Amir <abdullahamir.online@gmail.com>
2025-03-04 00:12:28 +0500

    New Code Pushed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,30 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello World");
+        Main_Sys sys = new Main_Sys("8xSys");
+        sys.printSysDetails();
+        sys.isWeConnectedWithGithubOnline();
+        System.out.println(sys.isWeConnectedWithGithubOnline());
+
+    }
+}
+
+
+
+class Main_Sys{
+    private String specification;
+    private boolean isOnlineConn;
+    Main_Sys(String specification){
+        this.specification = specification;
+    }
+
+    void printSysDetails(){
+        System.out.println(this.specification);
+    }
+
+
+    boolean isWeConnectedWithGithubOnline(){
+       return this.isOnlineConn = true;
     }
 }
 
