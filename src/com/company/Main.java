21cf2a36fb500949811c27b0e978f716d2f8ba02
Abdullah Amir <abdullahamir.online@gmail.com>
2025-03-03 22:56:55 +0500

    New Code Pushed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        System.out.println("Hello World");
     }
-}
-
-
-class System extends  Main{
-
 }
 
 
