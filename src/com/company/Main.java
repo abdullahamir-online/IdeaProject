e4e02bda58e87554a269d3b211c609c56046b55c
Abdullah Amir <abdullahamir.online@gmail.com>
2025-03-03 23:08:40 +0500

    New Code Pushed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,8 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello World");
+        Main_Sys sys = new Main_Sys("8xSys");
+        sys.printSysDetails();
     }
 }
 
@@ -18,6 +20,10 @@ class Main_Sys{
     String specification;
     Main_Sys(String specification){
         this.specification = specification;
+    }
+
+    void printSysDetails(){
+        System.out.println(this.specification);
     }
 }
 
